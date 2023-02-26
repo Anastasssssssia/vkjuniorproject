4cd3349098aac5fecac3a6b5ba3255349bc5b18d
Anastasssssssia <nasnaskolosova@gmail.com>
2023-02-26 23:35:30 +0300

    first commit

--- app/src/test/java/ExampleDialog.java
+++
@@ -1,9 +0,0 @@
-import android.app.Dialog;
-import android.support.v7.app.AppCompatDialogFragment;
-
-
-public class ExampleDialog {
-    
-    @Override 
-    public Dialog onCreateDialog(Bundle saved)
-}
